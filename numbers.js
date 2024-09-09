0f0c98d323f7abb38aadf718df115e2a33527625
afquidangen <alyssafayequidangen@gmail.com>
2024-09-09 12:15:48 +0800

    Variables Activity 2

---
+++ numbers.js
@@ -0,0 +1,24 @@
+let firstFavNum = 14;
+let secondFavNum = 5;
+
+let sum = firstFavNum  + secondFavNum;
+console.log(firstFavNum + " + " + secondFavNum + " = " + sum);
+//output: 14 + 5 = 19
+
+let difference = firstFavNum  -  secondFavNum;
+console.log(firstFavNum + " - " + secondFavNum + " = " + difference);
+//output: 14 - 5 = 9
+
+let product = firstFavNum  * secondFavNum;
+console.log(product);
+//output: 70
+
+let quotient = firstFavNum / secondFavNum;
+console.log(quotient);
+
+let modulus = firstFavNum % secondFavNum;
+console.log(modulus);
+
+let exponentiation = firstFavNum ** secondFavNum;
+console.log(exponentiation);
+
